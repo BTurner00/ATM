322e4dfe647da9f7e8b051633de3c619c4fa7400
Ben <bhturne@g.clemson.edu>
2016-05-19 15:09:06 -0400

    Day 4 Assignment - Added loops, create and remove account functionality, hashmap to store account info

--- src/com/theironyard/ATM.java
+++ src/com/theironyard/ATM.java
@@ -7,38 +7,63 @@ import static java.lang.Integer.valueOf;
  */
 public class ATM {
 
+    String acctChoice;
+    String name = Main.scanner.nextLine();
+    int flag = 1;
+
     public void chooseName() throws Exception {
-        String name = Main.scanner.nextLine();
+
+
 
         if (name.length()==0){
             throw new Exception("Or not.");
+        }
+        if (Main.accounts.get(name) == null) {
+            System.out.println("Account not found, would you like to create an account? [y/n]");
+            acctChoice = Main.scanner.nextLine();
+            if (acctChoice.equalsIgnoreCase("y")) {
+                Main.accounts.put(name, 0.00);
+            } else {
+                System.out.println("Please open an account to utilize this ATM. Thank you.");
+                flag = 0;
+            }
         }
 
     }
 
     public void chooseOption() throws Exception {
-        System.out.println("1. Check Balance");
-        System.out.println("2. Withdraw Funds");
-        System.out.println("3. Cancel");
-        System.out.println("Enter the number of your desired option:");
+        if (flag == 1){
+            String options="0";
+            while (!((options.equals("4")) || (options.equals("3")))) {
+            System.out.println("Enter the number of your desired option:");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw Funds");
+            System.out.println("3. Close Account");
+            System.out.println("4. Cancel");
 
-        String options = Main.scanner.nextLine();
 
-        if (options.equals("1")) {
-            System.out.println("Your balance is $100");
-        } else if (options.equals("2")) {
-            System.out.println("How much to withdraw?");
-            String amount = Main.scanner.nextLine();
-            int amountInt = valueOf(amount);
-            if (amountInt > 100) {
-                throw new Exception("Not enough funds.");
-            } else {
-                System.out.println("Please remove withdrawal from slot");
+            options = Main.scanner.nextLine();
+
+                if (options.equals("1")) {
+                    System.out.println("Your balance is $" + Main.accounts.get(name));
+                } else if (options.equals("2")) {
+                    System.out.println("How much to withdraw?");
+                    String amount = Main.scanner.nextLine();
+                    int amountInt = valueOf(amount);
+                    if (amountInt > Main.accounts.get(name)) {
+                        throw new Exception("Not enough funds.");
+                    } else {
+                        System.out.println("Please remove withdrawal from slot");
+                    }
+                } else if (options.equals("3")) {
+                    Main.accounts.remove(name);
+                    System.out.println("Account closed. Have a nice day.");
+                } else if (options.equals("4")) {
+                    System.out.println("Thank you. Please come again!");
+                } else {
+                    throw new Exception("Value entered not a valid option");
+                }
             }
-        } else if (options.equals("3")) {
-            System.out.println("Thank you. Please come again!");
-        } else {
-            throw new Exception("Value entered not a valid option");
         }
     }
 }
--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,18 +1,22 @@
 package com.theironyard;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 import static java.lang.Integer.valueOf;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    static HashMap<String, Double> accounts = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
-        System.out.println("What is your name?");
+        while(true) {
+            System.out.println("What is your name?");
 
-        ATM atm = new ATM();
-        atm.chooseName();
-        atm.chooseOption();
+            ATM atm = new ATM();
+            atm.chooseName();
+            atm.chooseOption();
+        }
     }
 
 }
