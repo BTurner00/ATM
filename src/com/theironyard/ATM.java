6b522db1e41815b8910aecb29de602d8950d9e7a
Ben <bhturne@g.clemson.edu>
2016-05-20 09:59:16 -0400

    Added Deposit function

--- src/com/theironyard/ATM.java
+++ src/com/theironyard/ATM.java
@@ -32,14 +32,16 @@ public class ATM {
     }
 
     public void chooseOption() throws Exception {
+        double deposit;
         if (flag == 1){
-            String options="0";
-            while (!((options.equals("4")) || (options.equals("3")))) {
+            String options = new String();
+            while (!((options.equals("4")) || (options.equals("5")))) {
             System.out.println("Enter the number of your desired option:");
             System.out.println("1. Check Balance");
             System.out.println("2. Withdraw Funds");
-            System.out.println("3. Close Account");
-            System.out.println("4. Cancel");
+                System.out.println("3. Deposit Funds");
+            System.out.println("4. Close Account");
+            System.out.println("5. Cancel");
 
 
             options = Main.scanner.nextLine();
@@ -49,16 +51,22 @@ public class ATM {
                 } else if (options.equals("2")) {
                     System.out.println("How much to withdraw?");
                     String amount = Main.scanner.nextLine();
-                    int amountInt = valueOf(amount);
-                    if (amountInt > Main.accounts.get(name)) {
+                    double amountDub = valueOf(amount);
+                    if (amountDub > Main.accounts.get(name)) {
                         throw new Exception("Not enough funds.");
                     } else {
                         System.out.println("Please remove withdrawal from slot");
+                        Main.accounts.put(name, (Main.accounts.get(name) - amountDub));
                     }
                 } else if (options.equals("3")) {
+                    System.out.println("Please enter deposit amount:");
+                    deposit = valueOf(Main.scanner.nextLine());
+                    Main.accounts.put(name, deposit);
+
+                } else if (options.equals("4")) {
                     Main.accounts.remove(name);
                     System.out.println("Account closed. Have a nice day.");
-                } else if (options.equals("4")) {
+                } else if (options.equals("5")) {
                     System.out.println("Thank you. Please come again!");
                 } else {
                     throw new Exception("Value entered not a valid option");
